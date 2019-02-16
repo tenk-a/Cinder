b4fd485f821123652008d3e82067d31ed5729878
tenk-a <tenka@6809.net>
2019-02-17 03:57:10 +0900

    Change variable name.

--- test/_opengl/CubeMapLayout/src/CubeMapLayoutApp.cpp
+++ test/_opengl/CubeMapLayout/src/CubeMapLayoutApp.cpp
@@ -19,13 +19,13 @@ class CubeMapLayoutApp : public App {
 	void mouseDown( MouseEvent event ) override;
 	void mouseDrag( MouseEvent event ) override;
 	void draw() override;
-	
+
 	std::vector<gl::TextureCubeMapRef> mCubeMaps;
 	int mSelectedCubeMap;
 	gl::BatchRef mCube;
 	params::InterfaceGlRef mParams;
 	CameraUi mMayaCam;
-	CameraPersp mMayaCamCamera;
+	CameraPersp mMayaCamPersp;
 };
 
 void CubeMapLayoutApp::prepareSettings( Settings * settings )
@@ -37,27 +37,27 @@ void CubeMapLayoutApp::setup()
 {
 	gl::enableDepthRead();
 	gl::enableDepthWrite();
-					
+
 	mParams = params::InterfaceGl::create( "Settings", ivec2(200, 200) );
 	mParams->addParam( "Cubemap", { "Horizontal Cross", "Vertical Cross", "Vertical Cross Layout", "Horizontal", "Vertical" }, &mSelectedCubeMap );
-	
+
 	gl::GlslProgRef shader = gl::GlslProg::create( loadAsset( "sky_box.vert" ), loadAsset( "sky_box.frag" ) );
 	shader->uniform( "uCubeMapTex", 0 );
-	
+
 	mCube = gl::Batch::create( geom::Cube().size( vec3( 100.0f ) ), shader );
-	
+
 	mSelectedCubeMap = 0;
 	mCubeMaps.push_back( gl::TextureCubeMap::create( loadImage( loadAsset( "horizontal_cross.jpg" ) ) ) );
 	mCubeMaps.push_back( gl::TextureCubeMap::create( loadImage( loadAsset( "vertical_cross.png" ) ) ) );
 	mCubeMaps.push_back( gl::TextureCubeMap::create( loadImage( loadAsset( "vertical_cross_layout.png" ) ) ) );
 	mCubeMaps.push_back( gl::TextureCubeMap::create( loadImage( loadAsset( "horizontal.png" ) ) ) );
 	mCubeMaps.push_back( gl::TextureCubeMap::create( loadImage( loadAsset( "vertical.hdr" ) ) ) );
-	
-	mMayaCamCamera = CameraPersp( getWindowWidth(), getWindowHeight(), 60.0, 0.1f, 1000.0f );
-	mMayaCamCamera.setEyePoint( vec3( 0, 0, 10 ) );
-	mMayaCamCamera.lookAt( vec3(0) );
-	
-	mMayaCam.setCamera( &mMayaCamCamera );
+
+	mMayaCamPersp = CameraPersp( getWindowWidth(), getWindowHeight(), 60.0, 0.1f, 1000.0f );
+	mMayaCamPersp.setEyePoint( vec3( 0, 0, 10 ) );
+	mMayaCamPersp.lookAt( vec3(0) );
+
+	mMayaCam.setCamera( &mMayaCamPersp );
 }
 
 void CubeMapLayoutApp::mouseDown( MouseEvent event )
@@ -73,13 +73,13 @@ void CubeMapLayoutApp::mouseDrag( MouseEvent event )
 void CubeMapLayoutApp::draw()
 {
 	gl::clear( Color( 0, 0, 0 ) );
-	
+
 	{
 		gl::setMatrices( mMayaCam.getCamera() );
 		gl::ScopedTextureBind texture( mCubeMaps[mSelectedCubeMap] );
 		mCube->draw();
 	}
-	
+
 	mParams->draw();
 }
 
